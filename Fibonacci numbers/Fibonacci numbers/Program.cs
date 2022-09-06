811eba97f4a56c96c32dacbefe876ca781d239f5
vetrikdavid <56035067+vetrikdavid@users.noreply.github.com>
2022-09-06 14:24:47 +0200

    Fibonacci Numbers
    N lenght fibonacci number value

---
+++ Fibonacci numbers/Fibonacci numbers/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            start();
+            fibonacciNum();
+            Console.ReadKey();
+        }
+
+        public static void start()
+        {
+            Console.WriteLine("Insert Fibonacci lenght: ");
+        }
+
+        public static int insertNum()
+        {
+            int num;
+            string input;
+            input = Console.ReadLine();
+            num = Convert.ToInt32(input);
+            return num;
+        }
+
+        public static void fibonacciNum()
+        {
+            int insernum = insertNum();
+            ulong prevnum = 0, nextnum = 1;
+            ulong finalnum = 0;
+            for (int i=1;i <= insernum; i++)
+            {
+                finalnum = prevnum + nextnum;
+                prevnum = nextnum;
+                nextnum = finalnum;
+
+                Console.WriteLine(i+" "+finalnum);
+            }
+        }
+    }
+}
